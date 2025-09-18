8dfcff980e76d56c7cdac0a0c5ef8e6505a03817
bart <bielski.bart1012@gmail.com>
2025-09-18 09:09:41 +0100

    fix slow load in search results

---
+++ App/ECP.UI/ECP.UI.Server/Models/ImageCaption.cs
@@ -0,0 +1,9 @@
+﻿namespace ECP.UI.Server.Models
+{
+    public class ImageCaption
+    {
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public string? ImgUrl { get; set; }
+    }
+}
